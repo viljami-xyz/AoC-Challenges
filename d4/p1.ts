eb6e854fc9342c4868b9b8af0b57de3fe3c0be6c
Viljami Kilkkilä <viljami.kilkkila@gmail.com>
2023-12-12 21:04:10 +0200

    Completed day 4 part 2

--- d4/p1.ts
+++ d4/p1.ts
@@ -1,5 +1,7 @@
 import readFile from '../utils/readFile';
 
+const cards: { [key: number]: number } = {};
+
 const filterCards = (cardString: string): number[] => {
     return cardString.trim().replace(/\s+/g, ' ')
         .split(' ').map((strNum) => parseInt(strNum, 10));
@@ -17,32 +19,68 @@ const getCardsInTuple = (line: string): [number[], number[]] => {
 
 };
 
-const countWins = (winCards: number[], myCards: number[]): number => {
+const countWins = (winCards: number[], myCards: number[]):
+    [number, number] => {
     const commonNumber: number[] = myCards.filter(
         (num) => winCards.includes(num));
-    if (commonNumber.length != 0) {
-        return 2 ** (commonNumber.length - 1);
+    const matches = commonNumber.length;
+    if (matches != 0) {
+        return [2 ** (matches - 1), matches];
     }
-    return 0;
+    return [0, 0];
 
 };
+
+const createCopies = (
+    currentCard: number,
+    copyAmount: number,
+    maxCards: number = 199) => {
+    for (let i = 0;
+        i < copyAmount; i++) {
+        if (currentCard + i > maxCards) {
+            return;
+        }
+        const thisCard = currentCard + i + 1;
+        if (thisCard in cards) {
+            cards[thisCard] += 1;
+        } else {
+            cards[thisCard] = 1;
+        }
+    }
+};
+
 
 const main = async () => {
     const fileContents = await readFile('./d4/input.txt');
     const lines = fileContents.split(/\r?\n/);
 
+    for (let i = 1; i < 200; i++) {
+        cards[i] = 1;
+    }
     const winPoints: number[] = [];
     for (let i = 0; i < lines.length; i++) {
         const line = lines[i];
+        const currentCard = i + 1;
         if (!line.startsWith('Card')) {
             continue
         }
         const [winCards, myCards] = getCardsInTuple(line);
-        winPoints.push(countWins(winCards, myCards));
+        const [matches, copies] = countWins(winCards, myCards);
+        if (copies > 0) {
+            for (let y = 0;
+                y < cards[currentCard]; y++) {
+                createCopies(currentCard, copies);
+            }
+            winPoints.push(matches);
+        }
 
-        
     };
     console.log(winPoints.reduce((acc, num) => acc + num, 0));
+    let allCards: number = 0;
+    for (let key in cards) {
+        allCards += cards[key];
+    };
+    console.log(allCards);
 
 };
 
