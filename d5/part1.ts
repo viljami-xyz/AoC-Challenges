faae09074a3b9599c1f7f9cee48f1c4bd32c0f6b
Viljami Kilkkilä <viljami.kilkkila@gmail.com>
2023-12-15 20:23:58 +0200

    Initial of day 5

---
+++ d5/part1.ts
@@ -0,0 +1,11 @@
+import readFile from '../utils/readFile';
+
+
+const inputFile = '../d5/input.txt';
+const getPlace = (data: string) => { };
+const mapNexPlace = (source: number, target: number) => { };
+
+const main = async () => {
+    const fileContent: string = await readFile(inputFile);
+
+}
